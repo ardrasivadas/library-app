4fd803d95eac8ffaf6fb19693b0ff3c33b500d94
ardrasivadas <ardrac15@gmail.com>
2024-06-07 13:07:20 +0530

    created

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
 
 const ViewAll = () => {
-  return (
-    <div>V</div>
-  )
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">PUBLISHED YEAR</th>
+                                    <th scope="col">TITLE</th>
+                                    <th scope="col">AUTHOR</th>
+                                    <th scope="col">PRICE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1999</th>
+                                    <td>WINGS OF FIRE</td>
+                                    <td>APJ ABDUL KALAM</td>
+                                    <td>800</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2003</th>
+                                    <td>HALF GIRLFRIEND</td>
+                                    <td>CHETAN BHAGAT</td>
+                                    <td>760</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2001</th>
+                                    <td>PRIDE AND PREJUDICE</td>
+                                    <td>JK ROWLING</td>
+                                    <td>790</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default ViewAll
