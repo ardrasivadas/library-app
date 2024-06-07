e7f8d2d8d356c0e6da3d39f4fc229d9d72627933
ardrasivadas <ardrac15@gmail.com>
2024-06-07 12:49:52 +0530

    created

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const AddBook = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">BOOK TITLE</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <lalel className="form-label">CATEGORY</lalel>
+                                <select name="" id="" className="form-control">
+                                    <option className="option">NOVEL</option>
+                                    <option className="option">SCIENCE</option>
+                                    <option className="option">FICTION</option>
+                                    <option className="option">AUTOBIOGRAPHY</option>
+                                </select>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">PUBLISHED YEAR</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">AUTHOR NAME</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">DESCRIPTION</label>
+                                <textarea name="" id="" className="form-control"></textarea>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">DISTRIBUTOR NAME</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">PUBLISHER NAME</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">PRICE</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">EDITION</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <button className="btn btn-success">SUBMIT</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddBook
